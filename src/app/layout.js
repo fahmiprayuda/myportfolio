2b27fd3968f3fceb7008be0a866f37bf13fadebb
Fahmi Prayuda <prayuda.fahmi@gmail.com>
2026-02-21 14:02:28 +0700

    first

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,16 @@
 import "./globals.css";
-import NavbarTesla from "@/components/NavbarTesla";
-import Footer from "@/components/Footer";
+
 
 export const metadata = {
-  title: "RSU Al-Arif Ciamis",
-  description: "Rumah Sakit Umum Tipe D di Ciamis dengan Layanan BPJS, USG 4D, Hemodialisa, dan Dokter Spesialis Berpengalaman",
+  title: "Nama Website Anda",
+  description: "Isi dengan deskripsi website Anda",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <NavbarTesla/>
         {children}
-        <Footer/>
         </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,10 @@
-import Navbar from "@/components/Navbar";
-import NavbarTesla from "@/components/NavbarTesla";
-import Footer from "@/components/Footer";
-import {CTASection} from "@/components/CTASection";
-import About from "@/components/About";
-import Service from "@/components/Service";
-import Contact from "@/components/Contact";
-import Trust from "@/components/Trust";
-import Portfolio from "@/components/Portfolio";
-import HeroApple from "@/components/HeroApple";
-import HeroTesla from "@/components/HeroTesla";
-import HeroNike from "@/components/HeroNike";
-import StickyWhatsApp from "@/components/StickyWhatsApp";
-import DoctorsPreview from "@/components/DoctorsPreview";
-
-export default function Home() {
+export default function Page()  {
   return (
     <>
-      <HeroApple />
-      <About />
-      <Trust />
-      <Service />
-      <DoctorsPreview />
-      <Portfolio />
-      <Contact/>
-      <CTASection />
-      <StickyWhatsApp />
-    </>
+    <div className="flex justify-center items-center mt-20 text-6xl text-red-500 font-bold">
+      <h1>Template 🚀</h1>
+    </div>
+    <p className="flex justify-center text-xl">This is a simple template for Next.js projects.</p>
+</>
   );
 }
